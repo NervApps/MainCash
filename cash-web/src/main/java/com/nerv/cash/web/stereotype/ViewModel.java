30c19ec3c55c5a151a0df6b451ce484119699618
Rosana <Rosana@Rosana-PC.home>
2015-09-28 22:29:37 -0300

    - Esteriótipos para definição dos managed beans

---
+++ cash-web/src/main/java/com/nerv/cash/web/stereotype/ViewModel.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nerv.cash.web.stereotype;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import javax.enterprise.inject.Stereotype;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+/**
+ * Esteriótipo para o beans request scoped.
+ * @author Vitor
+ */
+@Stereotype
+@Retention(RUNTIME)
+@Target({METHOD, FIELD, TYPE})
+@Named
+@ViewScoped
+public @interface ViewModel {}
